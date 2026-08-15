0516015c8ca52c31cca8f6ea77362397c1bfee91
felix_dev <dampfhans093@gmail.com>
2026-08-16 01:55:18 +0200

    first commit

---
+++ countdown.py
@@ -0,0 +1,14 @@
+import time
+
+print("input minutes an seconds")
+
+
+zahl = 3
+
+while zahl > 0:
+    print(zahl)
+    time.sleep(3)
+    zahl -= 1
+
+print("danke dass war mein timer, ich hoffe es hat dir gefallen, danke")
+   
